164435b2c6f0a0efbb442efa067110ad4fbb2c4c
Benjamin Oscar Aidoo <benaidoojnr@gmail.com>
2025-11-12 09:01:24 +0000

    Added Match Case Code

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,21 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+
+match operation:
+    case op if operation == "+":
+        result = num1 + num2
+        print("The result is" , result)
+    case op if operation == "-":
+        result = num1 - num2
+        print("The result is" , result)
+    case op if operation == "*":
+        result = num1 * num2
+        print("The result is" , result)
+    case op if operation == "/":
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            result = num1 - num2
+            print("The result is" , result)
+    
