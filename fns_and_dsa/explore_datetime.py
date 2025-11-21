c9776b0de46174ba7864992d67f8b8dd871461f5
Benjamin Oscar Aidoo <benaidoojnr@gmail.com>
2025-11-21 11:15:41 +0000

    Adding Date Time code

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,8 +1,8 @@
-import datetime
+from datetime import datetime
 
 
 def display_current_datetime():
-    current_date = datetime.datetime.now()
+    current_date = datetime.now()
     formatted = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print(formatted)
 
