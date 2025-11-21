8bbaadb00978dca5d3606dfff36e744619f7f3c6
Benjamin Oscar Aidoo <benaidoojnr@gmail.com>
2025-11-21 11:28:00 +0000

    Adding Date Time code

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,11 +1,19 @@
-from datetime import datetime
-from datetime import timedelta
+from datetime import datetime, timedelta, date
+
 
 
 def display_current_datetime():
     current_date = datetime.now()
     formatted = current_date.strftime("%Y-%m-%d %H:%M:%S")
-    print(formatted)
+    print(f"Current date and time: {formatted}")
 
 
+def calculate_future_date():
+    add_days = int(input("Enter the number of days to add to the current date: "))
+    future_date = date.today() + timedelta(days=add_days)
+    print(f"Future date: {future_date}")
+
+
+    
 display_current_datetime()
+calculate_future_date()
