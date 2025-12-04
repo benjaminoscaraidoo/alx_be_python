4f502949f59ef43f34786f0d8835be515ce58c93
Benjamin Oscar Aidoo <benaidoojnr@gmail.com>
2025-12-04 16:43:17 +0300

    Updated script for book class

---
+++ oop/book_class.py
@@ -0,0 +1,14 @@
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        return (f"Deleting (title of the book)")
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in ({self.year}"
+    
+    def __repr__(self):
+        return f"Book(title='{self.title}', author='{self.author}', {self.year})"
