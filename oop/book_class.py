333648e5de603ebd9987a0486715973a1b86fdd6
Benjamin Oscar Aidoo <benaidoojnr@gmail.com>
2025-12-04 16:59:51 +0300

    updated del method display for book class

--- oop/book_class.py
+++ oop/book_class.py
@@ -4,11 +4,17 @@ class Book:
         self.author = author
         self.year = year
 
-    def __del__(self):
-        return (f"Deleting {self.title}")
+
+
+    
     
     def __str__(self):
-        return f"{self.title} by {self.author}, published in ({self.year}"
+        return f"{self.title} by {self.author}, published in {self.year}"
+    
     
     def __repr__(self):
-        return f"Book(title='{self.title}', author='{self.author}', {self.year})"
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+    
+    
+    def __del__(self):
+        print(f"Deleting {self.title}")
